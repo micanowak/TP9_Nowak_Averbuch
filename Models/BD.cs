ecf431d41907fd296efa87c5567a115afd95af5f
MICAELA NOWAK <47297838@ort>
2022-10-13 12:09:58 -0300

    agregué muchas funciones en BD.cs de modificar y eliminar

--- Models/BD.cs
+++ Models/BD.cs
@@ -9,52 +9,57 @@ namespace TP9_Nowak_Averbuch.Models
     {
         private static string _ConnectionString = @"Server=A-PHZ2-CIDI-052;DataBase=JuegoQQSM;Trusted_Connection=True;";
 
-        public static void ReservarTurno(DateTime fechaIN, DateTime fechaOut, int fkHotel, int fkHabi, string Nombre, int dni){
-            string SQL = "INSERT INTO Reserva(fechaIN, fechaOUT, fkHotel, fkHabitacion, nombre, DNI) VALUES (@pfechaIN, @pfechaOUT, @pfkHotel, @pfkHabitacion, @pnombre, @pDNI)";
+        public static void ReservarHabitacion(DateTime fechaIN, DateTime fechaOut, int fkHotel, int fkHabi, string Nombre, int dni){
+            string SQL = "INSERT INTO Reserva(fechaIN, fechaOUT, fkHotel, fkHabitacion, nombre, DNI, comprobante) VALUES (@pfechaIN, @pfechaOUT, @pfkHotel, @pfkHabitacion, @pnombre, @pDNI)";
             using(SqlConnection db = new SqlConnection(_ConnectionString)){
                 db.Execute(SQL, new{pfechaIN = fechaIN, pfechaOUT = fechaOut, pfkHotel = fkHotel, pfkHabitacion = fkHabi, pnombre = Nombre, pDNI = dni});
             }
         }
 
-        public static void Disponibilidad(int idReserva){
-            Reserva posibleReserva = new Reserva();
-            bool seReserva = true;
-            int maxReservaID;
-            List<DateTime> ListaFechas = new List<DateTime>();
-            string SQL = "SELECT * FROM Reserva WHERE IdReserva = @pidReserva";
+        public static Reserva BuscarReserva(int idReserva){
+            Reserva dev = new Reserva();
+            string SQL = "SELECT * FROM Reserva WHERE IdReserva = @PidReserva";
             using(SqlConnection db = new SqlConnection(_ConnectionString)){
-                posibleReserva = db.QueryFirstOrDefault<Reserva>(SQL, new{pidReserva = idReserva});
+                dev = db.QueryFirstOrDefault<Reserva>(SQL, new{PidReserva = idReserva});
             }
-
-            SQL = "SELECT TOP 1 IdReserva FROM Reserva WHERE fkHotel = @pfkHotel and fkHabitacion = @pfkhab order by IdReserva DESC";
-            using(SqlConnection db = new SqlConnection(_ConnectionString)){
-                maxReservaID = db.QueryFirstOrDefault<int>(SQL, new{pfkHotel = posibleReserva.fkHotel, pfkhab = posibleReserva.fkHabitacion});
-            }
-            
-            for(int i = 1; i<= maxReservaID; i++){
-                SQL = "SELECT fechaIN,fechaOUT FROM Reserva WHERE fkHotel = @pfkHotel and fkHabitacion = @pfkhab";
-                using(SqlConnection db = new SqlConnection(_ConnectionString)){
-                ListaFechas = db.Query<DateTime>(SQL, new{pfkHotel = posibleReserva.fkHotel, pfkhab = posibleReserva.fkHabitacion}).ToList();
-            }
-
-            List<DateTime> listaNueva = new List<DateTime>();
-
-            for (int i2 = 0; i2 < ListaFechas.Count; i2++)
-            {
-                if (!(listaNueva.Contains(ListaFechas[i2])))
-                {
-                    listaNueva.Add(ListaFechas[i]);
-                }
-            }
-
-            if(ListaFechas.Count == listaNueva.Count){
-
-            }
-                SQL = "UPDATE Jugadores SET ComodinDobleChance = 0 WHERE IdJugador = @pIdJugador";
-                using(SqlConnection db = new SqlConnection(_ConnectionString)){
-                db.Execute(SQL, new{pIdJugador = _Player.IdJugador});
-                }
-            }
+            return dev;
         }
+
+        public static void ModificarFechaIN(DateTime fecha, int id){
+            string SQL = "UPDATE Reserva SET FechaIN = @pFecha WHERE IdReserva = @pID";
+                using(SqlConnection db = new SqlConnection(_ConnectionString)){
+                db.Execute(SQL, new{pFecha = fecha, pID = id});
+                }
+        }
+
+        public static void ModificarFechaOUT(DateTime fecha, int id){
+            string SQL = "UPDATE Reserva SET FechaOUT = @pFecha WHERE IdReserva = @pID";
+                using(SqlConnection db = new SqlConnection(_ConnectionString)){
+                db.Execute(SQL, new{pFecha = fecha, pID = id});
+                }
+        }
+
+        public static void ModificarHotel(int fkHot, int id){
+            string SQL = "UPDATE Reserva SET fkHotel = @pfkHot WHERE IdReserva = @pID";
+                using(SqlConnection db = new SqlConnection(_ConnectionString)){
+                db.Execute(SQL, new{pfkHot = fkHot, pID = id});
+                }
+        }
+
+        public static void ModificarHabitacion(int fkHab, int id){
+            string SQL = "UPDATE Reserva SET fkHabitacion = @pfkHab WHERE IdReserva = @pID";
+                using(SqlConnection db = new SqlConnection(_ConnectionString)){
+                db.Execute(SQL, new{pfkHab = fkHab, pID = id});
+                }
+        }
+
+        public static void EliminarReserva(int idReserva){
+            string SQL = "DELETE FROM Reserva WHERE IdReserva = @pID";
+                using(SqlConnection db = new SqlConnection(_ConnectionString)){
+                db.Execute(SQL, new{pID = idReserva});
+                }
+        }
+
+        // FALTA INGRESAR COMPROBANTE Y SUS FUNCIONES
     }
 }
--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -9,6 +9,8 @@ namespace TP9_Nowak_Averbuch.Models
         private int _fkHabitacion;
         private string _nombre;
         private int _DNI;
+        private string _Comprobante;
+        private bool _EstadoComprobante;
 
         public int IdReserva{ get {return _IdReserva;} set {_IdReserva = value;}}
         public DateTime fechaIN{ get {return _fechaIN;} set {_fechaIN = value;}}
@@ -17,6 +19,22 @@ namespace TP9_Nowak_Averbuch.Models
         public int fkHabitacion{ get {return _fkHabitacion;} set {_fkHabitacion = value;}}
         public string Nombre{ get {return _nombre;} set {_nombre = value;}}
         public int DNI{ get {return _DNI;} set {_DNI = value;}}
+        public string Comprobante{ get {return _Comprobante;} set {_Comprobante = value;}}
+        public bool EstadoComprobante{ get {return _EstadoComprobante;} set {_EstadoComprobante = value;}}
+
+
+        public Reserva(int idR, DateTime fechI, DateTime fechO, int hot, int hab, string nom, int dn1, string comp, bool estado){
+            
+            idR = _IdReserva;
+            fechI = _fechaIN;
+            fechO = _fechaOUT;
+            hot = _fkHotel;
+            hab = _fkHabitacion;
+            nom = _nombre;
+            dn1 = _DNI;
+            comp = _Comprobante;
+            estado = _EstadoComprobante;
+        }
 
         public Reserva(int idR, DateTime fechI, DateTime fechO, int hot, int hab, string nom, int dn1){
             
