40fb93c2de0cc5b4c4bf6a752ff21612ab7465b8
MIJAL MURIEL AVERBUCH <46754864@ort>
2022-10-13 09:20:13 -0300

    Creación del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP9_Nowak_Averbuch.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
