3f8e760cb4912439ac21cf8ec9126d4385fbce0f
MICAELA NOWAK <47297838@ort>
2022-10-13 10:19:58 -0300

    Agregué select, update y insert en bd.cs y creación de Reserva.cs

--- Models/BD.cs
+++ Models/BD.cs
@@ -9,6 +9,24 @@ namespace TP9_Nowak_Averbuch.Models
     {
         private static string _ConnectionString = @"Server=A-PHZ2-CIDI-052;DataBase=JuegoQQSM;Trusted_Connection=True;";
 
-        private;
+        public static void ReservarTurno(DateTime fechaIN, DateTime fechaOut, int fkHotel, int fkHabi, string Nombre, int dni){
+            string SQL = "INSERT INTO Reserva(fechaIN, fechaOUT, fkHotel, fkHabitacion, nombre, DNI) VALUES (@pfechaIN, @pfechaOUT, @pfkHotel, @pfkHabitacion, @pnombre, @pDNI)";
+            using(SqlConnection db = new SqlConnection(_ConnectionString)){
+                db.Execute(SQL, new{pfechaIN = fechaIN, pfechaOUT = fechaOut, pfkHotel = fkHotel, pfkHabitacion = fkHabi, pnombre = Nombre, pDNI = dni});
+            }
+        }
+
+        public static void Disponibilidad(int idReserva){
+            bool seReserva = true;
+            string SQL = "SELECT * FROM Reserva WHERE IdPregunta = @pidReserva";
+            using(SqlConnection db = new SqlConnection(_ConnectionString)){
+                posibleReserva = db.QueryFirstOrDefault<Reserva>(SQL, new{pidReserva = idReserva});
+            }
+            
+            string SQL = "UPDATE Jugadores SET ComodinDobleChance = 0 WHERE IdJugador = @pIdJugador";
+                using(SqlConnection db = new SqlConnection(_ConnectionString)){
+                db.Execute(SQL, new{pIdJugador = _Player.IdJugador});
+                }
+        }
     }
 }
