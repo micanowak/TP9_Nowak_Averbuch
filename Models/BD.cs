7d8673edd17b4e06e5a935a18265a04f52571a41
MICAELA NOWAK <47297838@ort>
2022-10-13 09:40:21 -0300

    Unión entre sql y el proyecto

---
+++ Models/BD.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Data.SqlClient;
+using Dapper;
+using System.Collections.Generic;
+
+namespace TP9_Nowak_Averbuch.Models
+{
+    class BD
+    {
+        private static string _ConnectionString = @"Server=A-PHZ2-CIDI-052;DataBase=JuegoQQSM;Trusted_Connection=True;";
+
+    }
+}
