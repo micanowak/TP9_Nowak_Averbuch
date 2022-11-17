333da4791fac917577c9a73220e19c17f39c90e1
MICAELA NOWAK <47297838@ort>
2022-11-17 12:01:50 -0300

    Esta todo terminado, solo falta foreach en el formulario y modal de ver habitaciones

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,27 +1,4 @@
-﻿
-function MostrarHotel(IdHotel) {
-    $.ajax({
-        type: 'POST',
-        dataType: 'JSON',
-        url: '/Home/VerDetalleActoresAjax',
-        data: { IdHotel: IdH },
-        success: function(response) {
-            $("#NombreHotel").html(response.nombre);
-            $("#StatusHabitacion").html("1. Habitación: " + response.statusNivel);
-            $("#StatusHabitacion").html("2. Habitación: " + response.statusNivel);
-            $("#StatusHabitacion").html("3. Habitación: " + response.statusNivel);
-
-        },
-        error: function(xhr, status) {
-            alert('Disculpe, existe un problema')
-        },
-        complete: function(xhr, status) {
-            console.log('Petición realizada');
-        }
-    });
-}
-
-function BuscarReserva() {
+﻿function BuscarReserva() {
     var Reserva = $("#Numero").val();
     $.ajax({
         type: 'POST',
